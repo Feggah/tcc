20762ad090c0f5638b95a9b39b5a7fb2d53962e4
Feggah <gabidferreira9@gmail.com>
2021-08-19 06:42:17 -0300

    Switch placeholder with the actual search widget
    Signed-off-by: Feggah <gabidferreira9@gmail.com>

---
+++ src/exposure/lib/presentation/view/widgets/home_search.dart
@@ -0,0 +1,41 @@
+import 'package:exposure/presentation/view/colors.dart';
+import 'package:exposure/presentation/view/widgets/secondary_text.dart';
+import 'package:flutter/material.dart';
+
+class HomeSearch extends StatelessWidget {
+  // ignore: avoid_field_initializers_in_const_classes
+  final textHint = "Quais lugares você visitou hoje?";
+
+  const HomeSearch() : super();
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      onPressed: () {},
+      child: Container(
+        height: MediaQuery.of(context).size.height / 18.5,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(27.0),
+          border: Border.all(
+            color: CustomColor.cardBorder,
+          ),
+        ),
+        padding: const EdgeInsets.only(left: 22),
+        child: Row(
+          children: [
+            const Icon(Icons.search),
+            SizedBox(
+              width: MediaQuery.of(context).size.width / 25,
+            ),
+            SecondaryText(
+              title: textHint,
+              size: 17,
+              center: false,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
