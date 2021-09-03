901899ad462bd2b58492acf69918d2647cfa6fad
Feggah <gabidferreira9@gmail.com>
2021-09-03 06:30:01 -0300

    Update router, injection and pubspecs
    Signed-off-by: Feggah <gabidferreira9@gmail.com>

--- src/exposure/lib/presentation/routes/router.gr.dart
+++ src/exposure/lib/presentation/routes/router.gr.dart
@@ -8,8 +8,9 @@ import 'package:auto_route/auto_route.dart' as _i1;
 import 'package:flutter/material.dart' as _i2;
 
 import '../view/home.dart' as _i3;
-import '../view/pages/infected_page.dart' as _i6;
-import '../view/pages/locations_page.dart' as _i5;
+import '../view/pages/details_page.dart' as _i5;
+import '../view/pages/infected_page.dart' as _i7;
+import '../view/pages/locations_page.dart' as _i6;
 import '../view/pages/search_page.dart' as _i4;
 
 class AppRouter extends _i1.RootStackRouter {
@@ -30,15 +31,26 @@ class AppRouter extends _i1.RootStackRouter {
         },
         opaque: true,
         barrierDismissible: false),
+    DetailsPageRoute.name: (routeData) => _i1.CustomPage<dynamic>(
+        routeData: routeData,
+        builder: (data) {
+          final pathParams = data.pathParams;
+          final args = data.argsAs<DetailsPageRouteArgs>(
+              orElse: () => DetailsPageRouteArgs(
+                  locationId: pathParams.getString('locationId')));
+          return _i5.DetailsPage(locationId: args.locationId);
+        },
+        opaque: true,
+        barrierDismissible: false),
     LocationsPageRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
         routeData: routeData,
         builder: (_) {
-          return _i5.LocationsPage();
+          return _i6.LocationsPage();
         }),
     InfectedPageRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
         routeData: routeData,
         builder: (_) {
-          return _i6.InfectedPage();
+          return _i7.InfectedPage();
         })
   };
 
@@ -48,7 +60,8 @@ class AppRouter extends _i1.RootStackRouter {
           _i1.RouteConfig(LocationsPageRoute.name, path: 'locations-page'),
           _i1.RouteConfig(InfectedPageRoute.name, path: 'infected-page')
         ]),
-        _i1.RouteConfig(SearchPageRoute.name, path: '/search-page')
+        _i1.RouteConfig(SearchPageRoute.name, path: '/search-page'),
+        _i1.RouteConfig(DetailsPageRoute.name, path: '/details-page')
       ];
 }
 
@@ -63,6 +76,21 @@ class SearchPageRoute extends _i1.PageRouteInfo {
   const SearchPageRoute() : super(name, path: '/search-page');
 
   static const String name = 'SearchPageRoute';
+}
+
+class DetailsPageRoute extends _i1.PageRouteInfo<DetailsPageRouteArgs> {
+  DetailsPageRoute({required String locationId})
+      : super(name,
+            path: '/details-page',
+            args: DetailsPageRouteArgs(locationId: locationId));
+
+  static const String name = 'DetailsPageRoute';
+}
+
+class DetailsPageRouteArgs {
+  const DetailsPageRouteArgs({required this.locationId});
+
+  final String locationId;
 }
 
 class LocationsPageRoute extends _i1.PageRouteInfo {
