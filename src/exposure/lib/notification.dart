7108615de98427117e9ec218512c179d8ad98a55
Feggah <gabidferreira9@gmail.com>
2021-09-18 19:00:10 -0300

    Add background notification handler implementation
    Signed-off-by: Feggah <gabidferreira9@gmail.com>

--- src/exposure/lib/notification.dart
+++ src/exposure/lib/notification.dart
@@ -1,7 +1,17 @@
 // Top-level functions to handle background notifications
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 Future firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  await Firebase.initializeApp();
+  try {
+    await Firebase.initializeApp();
+    final firestore = FirebaseFirestore.instance;
+    final auth = FirebaseAuth.instance;
+    final doc = firestore.collection("users").doc(auth.currentUser!.uid);
+    await doc.set({"lastNotified": DateTime.now().millisecondsSinceEpoch},
+        SetOptions(merge: true));
+    // ignore: empty_catches
+  } catch (e) {}
 }
